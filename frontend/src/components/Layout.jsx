5b24fcdfb8d0e454c4e2e88893bd9b62e85cdb19
masonliiu <liumasn@gmail.com>
2025-09-21 01:10:04 -0500

    parallax effect only in homepage and cropped

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -2,14 +2,16 @@ import Navbar from "./Navbar";
 import TriangleBackground from "../components/TriangleBackground";
 import "../components/TriangleBackground.css";
 import { useState, useEffect} from "react";
+import { useLocation } from "react-router-dom";
 
 export default function Layout({ children }) {
     const [offsetY, setOffsetY] = useState(0);
+    const location = useLocation();
 
     useEffect(() => {
         window.history.scrollRestoration = "manual";
         window.scrollTo(0, 0);
-    }, []);
+    }, [location.pathname]);
 
     useEffect(() => {
         const handleScroll = () => {
@@ -20,12 +22,14 @@ export default function Layout({ children }) {
     }, []);
     return (
         <div style={{ minHeight: "100vh", display: "flex", flexDirection: "column", position: "relative", overflowX: "hidden" }}>
-            <div
-                className="triangle-parallax"
-                style={{ transform: `translateY(${offsetY * -1.5}px)` }}
-            >
-                <TriangleBackground />
-            </div>
+            {location.pathname === "/" && (
+                <div
+                    className="triangle-parallax"
+                    style={{ transform: `translateY(${offsetY * -0.9}px)` }}
+                >
+                    <TriangleBackground />
+                </div>
+            )}
             <Navbar />
             <main style={{ flex: 1}} >
                 {children}
